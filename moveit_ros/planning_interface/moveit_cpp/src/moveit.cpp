0a9dd92fa012c662845d2d87829d59935bc0c1c9
Henning Kayser <henningkayser@picknik.ai>
2019-08-16 20:15:28 +0200

    Remove previous authors

--- moveit_ros/planning_interface/moveit_cpp/src/moveit.cpp
+++ moveit_ros/planning_interface/moveit_cpp/src/moveit.cpp
@@ -2,9 +2,6 @@
  * Software License Agreement (BSD License)
  *
  *  Copyright (c) 2019, PickNik Robotics
- *  Copyright (c) 2014, SRI International
- *  Copyright (c) 2013, Ioan A. Sucan
- *  Copyright (c) 2012, Willow Garage, Inc.
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -17,7 +14,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of Willow Garage nor the names of its
+ *   * Neither the name of PickNik Robotics nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
@@ -35,7 +32,7 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  *********************************************************************/
 
-/* Author: Ioan Sucan, Sachin Chitta, Simon Goldstien */
+/* Author: Simon Goldstien, Henning Kayser */
 
 #include <stdexcept>
 #include <sstream>
